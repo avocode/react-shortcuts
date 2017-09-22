3120e9d912a67ad2312aec5b091a0e9d2e58137d
Joe Fleming <joe.fleming@gmail.com>
2017-09-22 14:45:59 -0700

    create utils module
    this module contains all the helpers that lodash was being used for, except for reduce

---
+++ src/utils.js
@@ -0,0 +1,41 @@
+export const isArray = arr => Array.isArray(arr)
+
+export const isPlainObject = (obj) => {
+  return typeof obj === 'object' && obj !== null && !isArray(obj)
+}
+
+export const findKey = (obj, fn) => {
+  if (!isPlainObject(obj) && !isArray(obj)) return
+
+  const keys = Object.keys(obj)
+  return keys.find(key => fn.call(null, obj[key]))
+}
+
+export const compact = arr => arr.filter(Boolean)
+
+const flattenOnce = (arr, recurse = true) => {
+  return arr.reduce((acc, val) => {
+    if (isArray(val) && recurse) return acc.concat(flattenOnce(val, false))
+    acc.push(val)
+    return acc
+  }, [])
+}
+
+export const flatten = (arr) => {
+  if (!isArray(arr)) throw new Error('flatten expects an array')
+  return flattenOnce(arr)
+}
+
+export const map = (itr, fn) => {
+  if (isArray(itr)) return itr.map(fn)
+
+  const results = []
+  const keys = Object.keys(itr)
+  const len = keys.length
+  for (let i = 0; i < len; i += 1) {
+    const key = keys[i]
+    results.push(fn(itr[key], key))
+  }
+
+  return results
+}
